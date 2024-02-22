b6af5e4d741a87f5b485729e5b650ed05bb254b2
sod-5364395 <84495726+sod-5364395@users.noreply.github.com>
2024-02-22 16:48:24 +0100

    implemented function insert(data,index)

--- classes/linked_list.py
+++ classes/linked_list.py
@@ -24,8 +24,25 @@ class LinkedList(object):
         self.size += 1
 
     def insert(self, data, index: int) -> None:
-        # TODO: Implement function
-        pass
+        node = Node(data)
+        if index == self.size:
+            self.append(data)
+            # print("appended node")
+        elif index == 0:
+            node.set_next(self.head)
+            self.head = node
+            self.size += 1
+            # print("index is 0")
+        elif 0 < index < self.size:
+            # print("index between")
+            iter_node = self.head
+            for i in range(0, index-1):
+                iter_node = iter_node.get_next()
+            node.set_next(iter_node.get_next())
+            iter_node.set_next(node)
+            self.size += 1
+        else:
+            print(f"Given index is not valid. Current size of linked list is {self.size}.")
 
     def remove_data(self, data) -> None:
         if self.size == 0:
@@ -59,6 +76,8 @@ class LinkedList(object):
 
         index = 0
         node = self.head
+        print("____________________")
+        print("Current linked list:")
         while node is not None:
             print(f"Index {index}: {node.get_data()}")
             index += 1
@@ -66,27 +85,42 @@ class LinkedList(object):
 
 
 if __name__ == '__main__':
-    ll = LinkedList()
-    ll.print_ll_data()
-    print(ll.get_size())
-    ll.append(11)
-    print(ll.get_size())
-    ll.print_ll_data()
-    ll.append('bliblablup')
-    print(ll.get_size())
-    ll.print_ll_data()
-    ll.append(22)
-    ll.append('haha')
-    ll.append(33)
-    ll.print_ll_data()
-    ll.remove_data(22)
-    ll.print_ll_data()
-    ll.remove_data(44)
-    ll.remove_data('hahaha')
+    # ll = LinkedList()
+    # ll.print_ll_data()
+    # print(ll.get_size())
+    # ll.append(11)
+    # print(ll.get_size())
+    # ll.print_ll_data()
+    # ll.append('bliblablup')
+    # print(ll.get_size())
+    # ll.print_ll_data()
+    # ll.append(22)
+    # ll.append('haha')
+    # ll.append(33)
+    # ll.print_ll_data()
+    # ll.remove_data(22)
+    # ll.print_ll_data()
+    # ll.remove_data(44)
+    # ll.remove_data('hahaha')
 
-    ll2 = LinkedList()
-    ll2.remove_data(100)
-    ll2.append(100)
-    ll2.print_ll_data()
-    ll2.remove_data(100)
-    ll2.print_ll_data()
+    # ll2 = LinkedList()
+    # ll2.remove_data(100)
+    # ll2.append(100)
+    # ll2.print_ll_data()
+    # ll2.remove_data(100)
+    # ll2.print_ll_data()
+
+    ll3 = LinkedList()
+    ll3.insert(55, 0)
+    ll3.print_ll_data()
+    ll3.insert(66, 0)
+    ll3.print_ll_data()
+    ll3.insert(77, ll3.get_size())
+    ll3.print_ll_data()
+    ll3.insert(88, 1)
+    ll3.print_ll_data()
+    ll3.insert(99, 2)
+    ll3.print_ll_data()
+    ll3.insert(111, 6)
+    ll3.remove_data(99)
+    ll3.print_ll_data()
